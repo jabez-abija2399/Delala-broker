23768ef2ea0d6afe96f7b13f64dd6108e915f143
Ebenezerkifle <111049968+jabez-abija2399@users.noreply.github.com>
2023-08-23 11:13:59 -0700

    some files added

--- main/form.py
+++ main/form.py
@@ -6,12 +6,12 @@ from wtforms.fields import SelectMultipleField
 
 
 class RegistrationForm(FlaskForm):
-    FullName = StringField('Full Name', validators=[DataRequired(), Length(min=4, max=100)])
+    FullName = StringField('Full Name:', validators=[DataRequired(), Length(min=4, max=100)])
     # username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[Optional(), Email()])
-    phoneNumber = StringField('Phone Number', validators=[DataRequired(), Length(min=10, max=10)])
+    email = StringField('Email:', validators=[Optional(), Email()])
+    phoneNumber = StringField('Phone Number:', validators=[DataRequired(), Length(min=10, max=10)])
     gender = RadioField('Gender', choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')], validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('password:', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password:', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
