1ab90054ef9e248a06236c494e7513a0fa7be55a
Ebenezerkifle <111049968+jabez-abija2399@users.noreply.github.com>
2023-08-23 01:16:49 -0700

    form created

---
+++ main/form.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SelectField,RadioField,BooleanField,TextAreaField
+from wtforms.validators import DataRequired,Length,Email,EqualTo,Optional,ValidationError
+from wtforms.fields import SelectMultipleField
+
+
+
+class RegistrationForm(FlaskForm,UserMixin):
+    FullName = StringField('Full Name', validators=[DataRequired(), Length(min=4, max=100)])
+    # username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[Optional(), Email()])
+    phoneNumber = StringField('Phone Number', validators=[DataRequired(), Length(min=10, max=10)])
+    gender = RadioField('Gender', choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')], validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    
