af9e3ca4ed61bf6e44027f4d2a2b5aece17a9da6
Ebenezerkifle <111049968+jabez-abija2399@users.noreply.github.com>
2023-09-02 01:07:45 -0700

    file creted to run flask app

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from main import create_app
+from main.models import db
 
 app = create_app()
 
-if __name__ == '__main__':
-   app.run(debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+    app.run(debug=True)
