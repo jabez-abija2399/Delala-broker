1b0f96a979bf575bd5eb75a987e0fabee2c034cb
Ebenezerkifle <111049968+jabez-abija2399@users.noreply.github.com>
2023-09-14 19:38:15 -0700

    file updated

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# from main import create_app
+# from main.models import db
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+#         db.session.commit()
+#     app.run(debug=True)
+
+# wsgi.py
+
 from main import create_app
 from main.models import db
 
@@ -7,4 +20,3 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         db.session.commit()
-    app.run(debug=True)
